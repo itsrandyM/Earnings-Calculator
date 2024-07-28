03fb9f4fd561195ec20636387f390ac1de21099e
itsrandyM <randymungai20@gmail.com>
2024-07-28 20:33:25 +0300

    companies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AuthForm from "./Forms/authenticate";
 import AdminDashboard from "./dashboard/dash";
-import Companies from "./company/company";
+// import Companies from "./company/company";
 import IncomeEntry from "./Forms/income-entry";
 import UserDashboard from "./dashboard/dashboard";
 import AdminLogin from "./admin/login";
@@ -15,7 +15,7 @@ function App() {
             <Route path='/' element={<AuthForm/>}></Route>
             <Route path='/admin-login' element={<AdminLogin/>}></Route>
             <Route path='/admin-dashboard' element={<AdminDashboard/>}></Route>
-            <Route path='/companies' element={<Companies/>}></Route>
+            {/* <Route path='/companies' element={<Companies/>}></Route> */}
             <Route path="/income-entry" element={<IncomeEntry />} />
             <Route path="/dash-user" element={<UserDashboard />} />
         </Routes>
