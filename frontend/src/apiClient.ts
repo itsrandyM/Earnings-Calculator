51fa501bfa569bec4c4f9d8a3797239883a881d9
itsrandyM <randymungai20@gmail.com>
2024-07-29 10:58:15 +0300

    swithched up base backend link

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:4000', 
+  baseURL: 'https://earnings-calculator-backend.vercel.app', 
 });
 
 apiClient.interceptors.request.use(
