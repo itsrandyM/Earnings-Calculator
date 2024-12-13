26c730e5dbf2ca33a3201fb7bcbfec0b3f24baf1
Randy Mungai <134689399+itsrandyM@users.noreply.github.com>
2024-12-13 06:44:34 +0300

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,10 +1,11 @@
 const allowedOrigins = [
+    'https://earnings-calculator.onrender.com',
+    'https://earnings-calculator-2.onrender.com',
     'http://localhost:4000',
     'https://earnings-calculator-backend.vercel.app/',
     'http://localhost:5173',
-    'https://earnings-calculator-2.onrender.com',
     'https://earnings-calculator.vercel.app/',
-    'https://earnings-calculator.onrender.com'
+    
 ]
 
 module.exports = allowedOrigins
